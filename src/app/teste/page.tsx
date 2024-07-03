69dcda11218bccec7866a197cd554c880c82037c
Anthoni Campos <anthonithomes@gmail.com>
2024-07-03 12:28:15 -0300

    Shadcn/UI adicionado

--- src/app/teste/page.tsx
+++ src/app/teste/page.tsx
@@ -2,6 +2,7 @@
 
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { Button } from "@/components/ui/button";
 
 export default function Login() {
   const router = useRouter();
