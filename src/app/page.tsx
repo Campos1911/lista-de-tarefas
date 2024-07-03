ca3bd4209c6cfcddde911be0f2a36804c00b7dbf
Anthoni Campos <anthonithomes@gmail.com>
2024-07-03 11:50:01 -0300

    feature: Cor baseado no status da tarefa adicionada

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,6 +15,7 @@ export default function Home() {
             titulo={nota.titulo}
             dataFinal={nota.dataFinal}
             descricao={nota.descricao}
+            feito={nota.feito}
           />
         ))}
       </div>
@@ -27,25 +28,42 @@ const notas: NotasTypes[] = [
     titulo: "Título 1",
     dataFinal: "03/06/2024",
     descricao: "Breve descrição sobre a tarefa",
+    feito: true,
   },
   {
     titulo: "Título 2",
     dataFinal: "03/02/2024",
     descricao: "Breve descrição sobre a tarefa",
+    feito: false,
   },
   {
     titulo: "Título 3",
     dataFinal: "03/06/2024",
     descricao: "Breve descrição sobre a tarefa",
+    feito: false,
   },
   {
-    titulo: "Título 1",
+    titulo: "Título 4",
     dataFinal: "03/06/2024",
     descricao: "Breve descrição sobre a tarefa",
+    feito: false,
   },
   {
-    titulo: "Título 1",
+    titulo: "Título 5",
     dataFinal: "03/06/2024",
     descricao: "Breve descrição sobre a tarefa",
+    feito: false,
+  },
+  {
+    titulo: "Título 6",
+    dataFinal: "03/06/2024",
+    descricao: "Breve descrição sobre a tarefa",
+    feito: true,
+  },
+  {
+    titulo: "Título 7",
+    dataFinal: "03/06/2024",
+    descricao: "Breve descrição sobre a tarefa",
+    feito: true,
   },
 ];
--- src/components/Notas/Notas.tsx
+++ src/components/Notas/Notas.tsx
@@ -2,12 +2,29 @@ export interface NotasTypes {
   titulo: string;
   dataFinal: string;
   descricao: string;
+  feito: boolean;
 }
 
-export default function Notas({ titulo, dataFinal, descricao }: NotasTypes) {
+export default function Notas({
+  titulo,
+  dataFinal,
+  descricao,
+  feito,
+}: NotasTypes) {
+  let cor = "";
+
+  if (feito === true) {
+    cor = "green";
+  } else if (feito === false) {
+    cor = "red";
+  }
+
   return (
     <div className="w-[100%] flex flex-col gap-2 bg-slate-900 max-h-56 overflow-hidden px-5 py-3 rounded-md">
-      <h1 className="text-white text-2xl">{titulo}</h1>
+      <div className="w-[100%] flex items-center justify-between">
+        <h1 className="text-white text-2xl">{titulo}</h1>
+        <div className={`w-3 h-3 rounded-full bg-${cor}-500`}></div>
+      </div>
       <h2 className="text-gray-400">Data final: {dataFinal}</h2>
       <p className="text-gray-300">{descricao}</p>
     </div>
