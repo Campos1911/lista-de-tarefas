7dd753d2c755fdb630899c382811146388368785
Anthoni Campos <anthonithomes@gmail.com>
2024-07-04 20:53:17 -0300

    feature: Login com o Supabase funcionando

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,6 +2,8 @@
 
 import { Toaster } from "@/components/ui/toaster";
 import { useToast } from "@/components/ui/use-toast";
+import { createClient } from "@/utils/supabase/client";
+import { revalidatePath } from "next/cache";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
@@ -11,17 +13,28 @@ export default function Login() {
   const [email, setEmail] = useState<string>("");
   const [senha, setSenha] = useState<string>("");
 
-  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+  async function login(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    if (email !== senha) {
-      router.push("/quadro-geral");
-    } else {
+    const supabase = createClient();
+
+    // type-casting here for convenience
+    // in practice, you should validate your inputs
+    const data = {
+      email: email,
+      password: senha,
+    };
+
+    const { error } = await supabase.auth.signInWithPassword(data);
+
+    if (error) {
       toast({
         title: "Erro",
         description: "Usuário não encontrado!",
         duration: 3000,
         className: "bg-red-500 text-white border-none",
       });
+    } else {
+      router.push("/quadro-geral");
     }
   }
 
@@ -31,7 +44,7 @@ export default function Login() {
       <div className="w-[32%] bg-slate-900 shadow-md p-8 rounded-md">
         <h1 className="text-3xl text-center">Quadro de Notas</h1>
 
-        <form onSubmit={handleSubmit} className="mt-5 flex flex-col gap-3">
+        <form onSubmit={login} className="mt-5 flex flex-col gap-3">
           <label>
             <h3>Nome de usuário:</h3>
             <input
