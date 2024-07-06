32791ca603e2c10cf7563ff057bf9252c0181675
Anthoni Campos <anthonithomes@gmail.com>
2024-07-06 10:02:39 -0300

    feature: Transformando a string para receber o endereço correto na rota

--- src/components/Notas/Notas.tsx
+++ src/components/Notas/Notas.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export interface NotasTypes {
   titulo: string;
   dataFinal: string;
@@ -19,14 +21,23 @@ export default function Notas({
     cor = "bg-red-500";
   }
 
+  function transformString(input: string): string {
+    return input.replace(/ /g, "-");
+  }
+
+  const transformedString = transformString(titulo);
+
   return (
-    <div className="w-[100%] flex flex-col gap-2 shadow-md bg-slate-900 max-h-56 overflow-hidden px-5 py-3 rounded-md hover:scale-95 cursor-pointer duration-200">
+    <Link
+      href={`/quadro-geral/${transformedString}`}
+      className="w-[100%] flex flex-col gap-2 shadow-md bg-slate-900 max-h-56 overflow-hidden px-5 py-3 rounded-md hover:scale-95 cursor-pointer duration-200"
+    >
       <div className="w-[100%] flex items-center justify-between">
         <h1 className="text-white text-2xl">{titulo}</h1>
         <div className={`w-3 h-3 rounded-full ${cor}`}></div>
       </div>
       <h2 className="text-gray-400">Data final: {dataFinal}</h2>
       <p className="text-gray-300">{descricao}</p>
-    </div>
+    </Link>
   );
 }
